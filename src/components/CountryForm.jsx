cf3befaeb7d933d66dd0e6f22a8d5432194e592f
dpastoriza9 <dpastoriza@mobilecomputing.com.ar>
2023-04-19 18:39:33 -0300

    code update

--- src/components/CountryForm.jsx
+++ src/components/CountryForm.jsx
@@ -8,7 +8,7 @@ const CountryForm = ({ handleSearcher, handleContinent, searcher, continent }) =
         <div className='flex flex-col lg:flex-row items-start gap-7 lg:justify-between'>
             <input value={searcher} name="searcher" onInput={handleSearcher} className='custom-input w-full mb-7 lg:w-[525px]' type='text' placeholder='Search for a country...' />
             <select value={continent} name="continent" onChange={handleContinent} className='custom-input custom-select w-1/2 lg:w-[300px]'>
-                <option value="">Seleccionar un continent</option>
+                <option value="">Filter by Region</option>
                 {REGIONS.map((region) => (
                     <option key={uuidv4()} value={region.value}>{region.name}</option>
                 ))}
